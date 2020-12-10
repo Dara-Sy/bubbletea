818d9088569b8ff539146f144eeaa12e9ba23632
Dara-Sy <dara.sy@intersection.com>
2020-12-10 18:16:39 -0500

    clean up code

--- sketch.js
+++ sketch.js
@@ -1,29 +1,8 @@
-/* Bubble Tea Final Project - February 9, 2018
-Created by Dara Sy
-
-Movements of the bubbles were inspired by 
-https://p5js.org/examples/objects-array-of-objects.html (a P5 example)
-
-The structure of the constructor function is followed by Dan Shiffman's teaching here: 
-https://www.youtube.com/watch?v=F3GeM_KrGjI 
-
-I am inspired to create Bubble Tea, using a combination of objects, arrays, and constructor function. 
-
-My blog post: https://thejourneytocoding.wordpress.com/2018/02/08/final-project-taste-the-bubble-tea/ 
-
-*/ 
-
 // arrays 
 var menu = ["Taro", "Thai", "Lychee", "Milk Tea", "Earl Grey"] // array for flavor names 
 var colors = ["lavender", "orange", "pink", "ivory", "linen"]; // array for flavor colors in the cup 
 var bubbles = []; // empty array for the random positions of the bubbles in the cup 
 var index = 0; // the start of the index of arrays, index counts from 0 
-
-// global variables
-var w = 710; // the width of the canvas 
-var h = 400; // the height of the canvas 
-var fr = 10; // the frame rate to see the bubbles move and change color 
-var diam = 18; // the size of the bubbles 
 
 //objects 
 var cup = { // this is the literal objection notation for the cup (quad)
@@ -42,19 +21,12 @@ var straw = { // this is the literal objection notation for the straw (rect)
   y: 70, 
   w: 25, 
   l: 100
-  
-}
-
-var words = { // this object literal notation includes a width and length for a text box, similar to a rectangle 
-  w: 50, // width
-  l: 200 //length 
 }
 
 function setup() {
-  createCanvas(w, h); 
-  frameRate(fr);
-    
-  }
+  createCanvas(710, 400); 
+  frameRate(10); // the frame rate to see the bubbles move and change color FPS
+}
 
 
 function draw() {
@@ -67,7 +39,7 @@ function draw() {
   rect(straw.x, straw.y, straw.w, straw.l); // this is the straw 
   fill("white"); // the text is color white 
   textSize(32); // the text is 32 in font size 
-  text(menu[index], words.w, words.l); // this references change in words from menu array for the flavors of tea 
+  text(menu[index], 50, 200); // this references change in words from menu array for the flavors of tea // text(str, w, l)
 
   for (var i = 0; i < 5; i++) { // for loop to identify the bubbles indices, calling a new instance for Bubble
    bubbles[i] = new Bubble();
@@ -92,20 +64,21 @@ function Bubble() {
   this.y = random(290, 290); // this defines the this.y 
   this.x2 = random(400, 300); // this builds the this variables for the bubbles 
   this.y2 = random(340, 290); // the bubbles are placed in different coordinates at random 
+  this.diam = 18; // the size of the bubbles 
+  this.speed = 1;
   
   this.display = function() { // this calls the function to display the bubbles 
     noStroke();
     fill(random(255), random(255), random(255));
-    ellipse(this.x, this.y, diam, diam);
-    ellipse(this.x2, this.y2, diam, diam);
-    
+    ellipse(this.x, this.y, this.diam, this.diam);
+    ellipse(this.x2, this.y2, this.diam, this.diam);
   }
   
   this.move = function() { // this calls the function to move the bubbles by 1 increment or 1 decrement at random coordinates 
-    this.x = this.x + random(-1, 1); // referring back to line 19, I chose the frame rate 10, so we can see the movement of bubbles 
-    this.y = this.y + random(-1, 1);
-    this.x2 = this.x2 + random(-1, 1);
-    this.y2 = this.y2 + random(-1, 1);
+    this.x += random(-this.speed, this.speed); // referring back to line 19, I chose the frame rate 10, so we can see the movement of bubbles 
+    this.y += random(-this.speed, this.speed);
+    this.x2 -= random(-this.speed, this.speed);
+    this.y2 -= random(-this.speed, this.speed);
   }
 } 
 
